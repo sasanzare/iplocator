8f7064e76c26b2042757136bfb57cc2e86cf996b
kener <kener@localhost.localdomain>
2020-01-21 19:54:44 +0330

    feat : Features
    Add 3 API
    https://api.ipdata.co/
    https://ipinfo.io/
    http://api.db-ip.com/v2/free/

--- python/iplocator.py
+++ python/iplocator.py
@@ -1,36 +1,56 @@
 #!/usr/bin/python3
-# iplocateor v 0.0.1 by @sasan zare 2020/01/06
+# iplocateor v 0.1.0 by @sasan zare 2020/01/21
+# API documentation https://api.ipdata.co/
 # API documentation http://ip-api.com/json/
+# API documentation https://ipinfo.io/
+# API documentation http://api.db-ip.com/v2/free/
+# importing the asyncio library
+import asyncio
 # importing the requests library
 import requests
-#Please input IP
 print(' Please input IP :')
 IP = input()
-# Api-endpoint
-URL = "http://ip-api.com/json/"+IP
-request = requests.get(url = URL)
-# Extracting data in json format
-data = request.json()
+# async function
+async def main():
+    loop = asyncio.get_event_loop()
+    res_1 = (await (loop.run_in_executor(None, requests.get, ("https://api.ipdata.co/"+IP+"?api-key=test")))).json()
+    res_2 = (await (loop.run_in_executor(None, requests.get, ("http://ip-api.com/json/"+IP)))).json()
+    res_3 = (await (loop.run_in_executor(None, requests.get, ("https://ipinfo.io/"+IP+"/geo")))).json()
+    res_4 = (await (loop.run_in_executor(None, requests.get, ("http://api.db-ip.com/v2/free/"+IP)))).json()
 #Made by sasan zare
-print('''================================+================================
-                _______  _______  _______  _______
-               (  ____ \(  ___  )/ ___   )(  ___  )
-               | (    \/| (   ) |\/   )  || (   ) |
-               | (_____ | (___) |    /   )| (___) |
-               (_____  )|  ___  |   /   / |  ___  |
-                     ) || (   ) |  /   /  | (   ) |
-               /\____) || )   ( | /   (_/\| )   ( |
-               \_______)|/     \|(_______/|/     \|
+    print('''   ================================+================================
+                    _______  _______  _______  _______
+                   (  ____ \(  ___  )/ ___   )(  ___  )
+                   | (    \/| (   ) |\/   )  || (   ) |
+                   | (_____ | (___) |    /   )| (___) |
+                   (_____  )|  ___  |   /   / |  ___  |
+                         ) || (   ) |  /   /  | (   ) |
+                   /\____) || )   ( | /   (_/\| )   ( |
+                   \_______)|/     \|(_______/|/     \|
 
- Ip locate
- By: @sasan zare
+     Ip locate
+     By: @sasan zare
 
-================================+================================ ''')
-#Create table
-from prettytable import PrettyTable
-table = PrettyTable()
-table.field_names = ["Country", "CountryCode","RegionName","City","Timezone","Isp"]
-table.add_row([data["country"], data["countryCode"], data["regionName"], data["city"],data["timezone"],data["isp"]])
+    ================================+================================
+    IP : '''+IP)
+    from prettytable import PrettyTable
+    table = PrettyTable()
+    table.field_names = ["Property", "API_1","API_2","API_3","API_4"]
+    table.add_row(["Country", res_1["country_name"],res_2["country"]," ",res_4["countryName"]])
+    table.add_row(["CountryCode", res_1["country_code"],res_2["countryCode"],res_3["country"],res_4["countryCode"]])
+    table.add_row(["City", res_1["city"],res_2["city"],res_3["city"],res_4["city"]])
+    table.add_row(["regionName", res_1["region"],res_2["regionName"],res_3["region"]," "])
+    table.add_row(["timezone", res_1["time_zone"]["name"],res_2["timezone"],res_3["timezone"]," "])
+    table.add_row(["continent_name", res_1["continent_name"]," "," ",res_4["continentName"]])
+    table.add_row(["Is_tor", res_1["threat"]["is_tor"]," "," ",""])
+    table.add_row(["Is_proxy", res_1["threat"]["is_proxy"]," "," ",""])
+    table.add_row(["Is_anonymous", res_1["threat"]["is_anonymous"]," "," ",""])
+    table.add_row(["Is_known_attacker", res_1["threat"]["is_known_attacker"]," "," ",""])
+    table.add_row(["Is_known_abuser", res_1["threat"]["is_known_abuser"]," "," ",""])
+    table.add_row(["Is_threat", res_1["threat"]["is_threat"]," "," ",""])
+    table.add_row(["Is_bogon", res_1["threat"]["is_bogon"]," "," ",""])
 # printing the output
-print(" IP : "+IP)
-print(table)
+    print(table)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
